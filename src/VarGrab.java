4cf9b5dce7c8f5b32dc36e91fec028182ebf2472
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-01-14 22:37:27 -0800

    fixed comment error

--- src/VarGrab.java
+++ src/VarGrab.java
@@ -25,7 +25,7 @@ public class VarGrab {
         // scan the text
         String input = "";
         String text = "";
-        while(scanner.hasNextLine()){
+        while (scanner.hasNextLine()) {
             input = scanner.nextLine();
             text += input + "\n";
         }
@@ -35,55 +35,53 @@ public class VarGrab {
         System.out.println(text);
 
         // Make the pattern that we will use to search for in our Reg Exp
-            // first reg exp will grab subset the text to give me the entire
-            // row where it finds variable identifiers
-            // then I will further break that apart for type, name, value
+        // first reg exp will grab subset the text to give me the entire
+        // row where it finds variable identifiers
+        // then I will further break that apart for type, name, value
         Pattern pattern = Pattern.compile("(int.*|double.*|float.*|char.*|String.*).*;");
         Matcher matcher = pattern.matcher(text);
         String subset_01 = "";
         int countMatch = 0;
         System.out.println("loop should start printing now...\n\n");
-        while( matcher.find() ){
+        while (matcher.find()) {
             System.out.println("\t" + matcher.group(1));
             subset_01 = subset_01 + "\n" + matcher.group(1);
             countMatch++;
         }
-            // preview subset_01
+        // preview subset_01
 //        System.out.println("\t" + subset_01);
 
 
-
-
-            // put every row into an array of strings for looping work later
+        // put every row into an array of strings for looping work later
         Pattern patternROW = Pattern.compile("(.*)\n");
         Matcher matcher2 = patternROW.matcher(subset_01);
         String[] arrSTRING = new String[countMatch];
         int i = 0;
         System.out.println("\nprinting second matcher...\n");
-        while( matcher2.find() ){
+        while (matcher2.find()) {
             System.out.println("\t\t" + matcher2.group(1));
             arrSTRING[i] = matcher2.group(1);
             i++;
         }
-            // double check the array of strings
+        // double check the array of strings
         System.out.println("\nprinting array list [1]...\n" + arrSTRING[1]);
         System.out.println("\nprinting array list [2]...\n" + arrSTRING[2]);
-        for (int g = 1; g < 12; g++){
+        for (int g = 1; g < 12; g++) {
             System.out.println("\t\t\t\t" + arrSTRING[g]);                  // <---------These rows fail ????
         }
 
 
-            // run for loop on this array of strings
-                // use 3 different patterns to extract type/name/value
+        // run for loop on this array of strings
+        // use 3 different patterns to extract type/name/value
         Pattern patternTYPE = Pattern.compile("(int.*|double.*|float.*|char.*|String.*)");
         Pattern patternNAME = Pattern.compile("\\s{1}([a-zA-Z]{1,}).*");
         Pattern patternVALUE = Pattern.compile("=\\s(.*)");
 
 //
-//        for (int j = 0; j < countMatch; j++){
-            Matcher matcherTYPE = patternTYPE.matcher( arrSTRING[i+1] );    // <---------These rows fail ????
-            Matcher matcherNAME = patternNAME.matcher( arrSTRING[i+1] );    // <---------These rows fail ????
-            Matcher matcherVALUE = patternVALUE.matcher( arrSTRING[i+1] );  // <---------These rows fail ????
+        for (int j = 0; j < countMatch; j++) {
+            Matcher matcherTYPE = patternTYPE.matcher(arrSTRING[i + 1]);    // <---------These rows fail ????
+            Matcher matcherNAME = patternNAME.matcher(arrSTRING[i + 1]);    // <---------These rows fail ????
+            Matcher matcherVALUE = patternVALUE.matcher(arrSTRING[i + 1]);  // <---------These rows fail ????
 
             System.out.println("Type: " + matcherTYPE.group(1));            // <---------These rows fail ????
             System.out.println("Name: " + matcherNAME.group(1));            // <---------These rows fail ????
@@ -92,8 +90,6 @@ public class VarGrab {
         }
 
 
-
-
     }
-
+}
 
